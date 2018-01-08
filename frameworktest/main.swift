0d4d534acb9121f4d8afb2204a11dfc36cce32e7
Jordan Campbell <jordan.evan.campbell@gmail.com>
2018-01-08 17:36:58 +1300

    Mon  8 Jan 2018 17:36:58 NZDT

---
+++ frameworktest/main.swift
@@ -0,0 +1,20 @@
+//
+//  main.swift
+//  frameworktest
+//
+//  Created by Jordan Campbell on 8/01/18.
+//  Copyright © 2018 Atlas Innovation. All rights reserved.
+//
+
+import Foundation
+
+
+public class Test {
+    init() {
+        print("Test init.")
+    }
+    
+    public func run() {
+        print("Test run.    ")
+    }
+}
